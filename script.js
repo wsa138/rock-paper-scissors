b566d46820e3b1085babcf1e8af7c93ca20db0cb
William Addesso <wsaddesso@gmail.com>
2020-10-10 15:31:21 -0400

    Added code to change text on user interface displaying wins and losses.

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-let rockButton = document.querySelector('#rock');
-let paperButton = document.querySelector('#paper');
-let scissorsButton = document.querySelector('#scissors');
+var won = 0;
+var lost = 0;
 
-rockButton.addEventListener("click", function() {
+document.querySelector('#rock').addEventListener("click", function() {
     game('rock');
 });
-paperButton.addEventListener("click", function() {
+document.querySelector('#paper').addEventListener("click", function() {
     game('paper');
 });
-scissorsButton.addEventListener("click", function() {
+document.querySelector('#scissors').addEventListener("click", function() {
     game('scissors');
 });
-
-
+document.getElementById('score').innerHTML = `Wins: ${won}   Losses: ${lost}`;
 
 
 function computerSelect() {
@@ -36,48 +34,45 @@ function playerSelect() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    //Takes user and computer input to play a round of the game
+    //Takes user and computer input to play a round of the game, display round results.
     if (playerSelection == "rock") {
         if (computerSelection == "scissors") {
             let result = (document.querySelector('#result').textContent = 'Winner!\
             Rock beats Scissors');
-            return result;
+            won++;
         } else if (computerSelection == "paper") {
             let result = (document.querySelector('#result').textContent = 'Loser, \
             Paper beats Rock');
-            return result;
+            lost++;
         } else if (computerSelection == "rock") {
             let result = (document.querySelector('#result').textContent = 'Tie! \
             You both picked Rock');
-            return result;
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
             let result = (document.querySelector('#result').textContent = 'Winner! \
             Paper beats Rock');
-            return result;
+            won++;
         } else if (computerSelection == "scissors") {
             let result = (document.querySelector('#result').textContent = 'Loser, \
             Scissors beats Paper');
-            return result;
+            lost++;
         } else if (computerSelection == "paper") {
             let result = (document.querySelector('#result').textContent = 'Tie! \
             You both picked Paper');
-            return result;
         }
     } else if (playerSelection == "scissors") {
         if (computerSelection == "paper") {
             let result = (document.querySelector('#result').textContent = 'Winner! \
             Scissors beats Paper');
-            return result;
+            won++;
         } else if (computerSelection == "rock") {
             let result = (document.querySelector('#result').textContent = 'Loser, \
             Rock beats Scissors');
-            return result;
+            lost++;
         } else if (computerSelection == "scissors") {
             let result = (document.querySelector('#result').textContent = 'Tie! \
             You both picked Scissors');
-            return result;
         }
     }
 }
@@ -89,36 +84,12 @@ function game(pick) {
     losses = 0;
     ties = 0;
     for (i = 0; i < 1; i++) {
-        //Loops playRound() five times//
+        //Loops playRound(), keeps track of wins.
         let player1 = pick;
         let computer1 = computerSelect();
         let round = playRound(player1, computer1);
-        incrementScore(round);
-    }
-    displayWinner(wins, losses)      
+        document.getElementById('score').innerHTML = `Wins: ${won}  Losses: ${lost}`;
+    }   
 }
 
-function displayWinner(w, l) {
-    //Takes wins and losses record and displays the winner//
-    if (w > l) {
-        console.log("You are the Winner!");
-    } else if (w < l) {
-        console.log("You lost");
-    } else {
-        console.log("It is a tie!");
-    }
-}
 
-function incrementScore(s) {
-    //Increments wins, losses or ties.
-    if (s.charAt(0) === "W") {
-        wins += 1;
-        console.log(s);
-    } else if (s.charAt(0) === "L") {
-        losses += 1;
-        console.log(s);
-    } else if (s.charAt(0) === "T") {
-        ties += 1;
-        console.log(s);
-    }
-}
